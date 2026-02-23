bf32e8932560e6ed1d02fc0b2c097b516fae931b
theram600-arch <theram600@gmail.com>
2026-02-23 12:26:12 +0530

    make changes in code

--- Day1.py
+++ Day1.py
@@ -648,11 +648,6 @@
 # print(fruits)
 # fruits.sort(reverse=True)
 # print(fruits)
-# ages = [22, 19, 24, 25, 26, 24, 25, 24]
-# ages.sort()
-# print(ages)
-# ages.sort(reverse=True)
-# print(ages)
 
 
 
