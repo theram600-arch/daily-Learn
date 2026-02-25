6ad9a76fd96fdb85b3dbb2a3a8e7d4f5eedd5187
theram600-arch <theram600@gmail.com>
2026-02-25 20:04:30 +0530

    Learn loops and practice some programs

---
+++ Day4_loops.py
@@ -0,0 +1,45 @@
+# loops    Loops are programming constructs that repeat a specific set of instructions multiple times until a defined condition is met
+
+# fruits=["apple","banana","cocunot","guava"]
+
+# for i in fruits:
+#     print(i)
+#     if i=="banana":
+#         break
+
+
+# for i in range(3):
+#     for j in range(2):
+#         print(i, j)
+
+
+# fruits=[(3,1),(5,6),(8,0)]
+
+# for x in fruits:
+#     print(f"{x=}and {y=}")
+
+
+
+# i=0
+# while i <len(numbers):
+#     print(numbers[i])
+#     i+=1
+
+
+# fruits = ["orange", "apple", "mango", "lemon"]
+
+# for i in range(len(fruits)):
+#     fruit=fruits[i]
+#     print(i,fruit)
+
+
+# t=0
+# u=0
+# for i in range(1,101):
+#     if i%2==0:
+#         u=u+i
+#         print(u)
+# for j in range(0,101):
+#     if j%2!=0:
+#         t=t+j
+#         print(j)
