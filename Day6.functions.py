8dced1ec4a28ff661a5f121eabea046af6a70d69
theram600-arch <theram600@gmail.com>
2026-03-09 16:55:41 +0530

    learn what are functions and practice some questions

---
+++ Day6.functions.py
@@ -0,0 +1,67 @@
+# def hello():
+#     print("Hello World")
+
+# hello()
+
+
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Ram")
+
+
+# # add two numbers
+# def add(a, b):
+#     return a + b
+
+# result = add(5, 7)
+# print(result)
+
+
+
+# even and odd numbers
+# def check_even_odd(num):
+#     if num % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+
+# print(check_even_odd(8))
+
+
+
+
+# # find maximum number
+# def find_max(a, b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+
+# print(find_max(10, 25))
+
+
+
+# # # factorial function
+# def factorial(n):
+#     result = 1
+#     for i in range(1, n + 1):
+#         result = result * i
+#     return result
+
+# print(factorial(5))
+
+
+
+# # count vowels
+# def count_vowels(text):
+#     vowels = "aeiouAEIOU"
+#     count = 0
+    
+#     for char in text:
+#         if char in vowels:
+#             count += 1
+    
+#     return count
+
+# print(count_vowels("hello"))
