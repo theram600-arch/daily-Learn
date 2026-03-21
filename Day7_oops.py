2ef5fcf89d3fedf82dea79ee5057fb0158752baf
theram600-arch <theram600@gmail.com>
2026-03-21 17:28:15 +0530

    going to learn oops concepts

---
+++ Day7_oops.py
@@ -0,0 +1,35 @@
+# # class dog:
+# #     def __init__(self,name):
+# #         self.name=name
+# # p=dog("monty")
+# # print(p.name)
+
+
+
+# class Person:
+#     def __init__(self, firstname, lastname, age, country, city):
+#           self.firstname = firstname
+#           self.lastname = lastname
+#           self.age = age
+#           self.country = country
+#           self.city = city
+#           self.skills=[]
+    
+#     def person_info(self):
+#         return f'{self.firstname} {self.lastname} is {self.age} years old. He lives in {self.city}.'
+#     def pun(self,skill):
+#          self.skills.append(skill)
+         
+    
+# # class child(Person):
+# #     def sp(self,skills):
+# #          return f"my name is {self.firstname}. and i am {self.age} years old. "
+
+
+# p = Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+
+# p.pun("javascript")
+# print(p.skills)
+# print(p.firstname)
+# print(p.age)
+# print(p.person_info())
